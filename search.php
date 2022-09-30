eef3ff6d0c7e7b7565bad99b6f60bd3a58b0b8f1
Pablo Ramirez <pabloramirez@Gabriels-MacBook-Air.local>
2022-09-30 13:36:45 -0500

    search page

--- search.php
+++ search.php
@@ -1,26 +1,28 @@
 <?php get_header(); ?>
-<?php
-$s = get_search_query();
-$args = array(
-    's' => $s
-);
-// The Query
-$the_query = new WP_Query($args);
-if ($the_query->have_posts()) {
-    _e("<h2 style='font-weight:bold;color:#000'>Search Results for: " . get_query_var('s') . "</h2>");
-    while ($the_query->have_posts()) {
-        $the_query->the_post();
-?>
-        <li>
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-        </li>
+<div class="contenedor">
     <?php
-    }
-} else {
+    $s = get_search_query();
+    $args = array(
+        's' => $s
+    );
+    // The Query
+    $the_query = new WP_Query($args);
+    if ($the_query->have_posts()) {
+        _e("<h2 style='font-weight:bold;color:#000'>Search Results for: " . get_query_var('s') . "</h2>");
+        while ($the_query->have_posts()) {
+            $the_query->the_post();
     ?>
-    <h2 style='font-weight:bold;color:#000'>Nada encontrado</h2>
-    <div class="alert alert-info">
-        <p>No hay nada relacionado con tu busqueda.</p>
-    </div>
-<?php } ?>
+            <li>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+        <?php
+        }
+    } else {
+        ?>
+        <h2 style='font-weight:bold;color:#000'>Nada encontrado</h2>
+        <div class="alert alert-info">
+            <p>No hay nada relacionado con tu busqueda.</p>
+        </div>
+    <?php } ?>
+</div>
 <?php get_footer(); ?>
