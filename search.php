61c6f984d2b972e0324f2ed87548a997365b8a2b
Pablo Ramirez <pabloramirez@Gabriels-MacBook-Air.local>
2022-09-30 13:33:20 -0500

    custom home

--- search.php
+++ search.php
@@ -7,13 +7,24 @@ Template Name: Search Page
 get_header(); ?>
 
 <div class="contenedor">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-			<?php get_search_form(); ?>
+            <?php get_search_form(); ?>
+            <?php
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+            global $query_string;
+
+            wp_parse_str($query_string, $search_query);
+            $search = new WP_Query($search_query);
+
+            ?>
+            <?php
+            global $wp_query;
+            $total_results = $wp_query->found_posts;
+            ?>
+        </main><!-- #main -->
+    </div><!-- #primary -->
 </div><!-- .wrap -->
 
 <?php get_footer();
