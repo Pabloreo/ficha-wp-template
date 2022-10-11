1518cdcb693080c6b7f71a2391913ccc2adcef98
Pablo Ramirez <pabloramirez@192.168.1.3>
2022-10-11 17:40:25 -0500

    icon cart, search header

--- search.php
+++ search.php
@@ -3,12 +3,13 @@
     <?php
     $s = get_search_query();
     $args = array(
-        's' => $s
+        's' => $s,
+        'posts_per_page' => -1
     );
     // The Query
     $the_query = new WP_Query($args);
     if ($the_query->have_posts()) {
-        _e("<h2 style='font-weight:bold;color:#000'>Search Results for: " . get_query_var('s') . "</h2>");
+        _e("<h2 style='font-weight:bold;color:#000'>Resultados para: " . get_query_var('s') . "</h2>");
         while ($the_query->have_posts()) {
             $the_query->the_post();
     ?>
